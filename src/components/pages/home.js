cc9d5871bc06b34fa1df467c430d4b25416b1909
Shavidini Ekanayake <77123486+ShavidiniEkanayake@users.noreply.github.com>
2023-02-21 13:11:56 +0530

    Created insert UI

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,25 +1,88 @@
 import React from "react";
-import { Modal, InputNumber, Space,Button } from "antd";
+import { Button, Input } from "antd";
 import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 import axios from "axios";
+import "react-notifications/lib/notifications.css";
+import "../../card.css";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
 
-const All = () => {
-    return (
-    <div>aaa</div>
-    
-    )
-}
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
+
+const NewGass = () => {
+  return (
+    <div className="mt-20" style={{ marginLeft: "32rem" }}>
+      <NotificationContainer></NotificationContainer>
+      <Card
+        style={{
+          width: "30rem",
+          marginTop: "11rem",
+          marginBottom: "11rem",
+          background: "",
+        }}
+      >
+        <Card.Body>
+          <Card.Title style={{ marginLeft: "11rem" }}>Add</Card.Title>
+
+          <div className="my-6 mx-9">
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              style={{ marginBottom: 15 }}
+              placeholder=""
+              class="form-control"
+              required
+            />
+
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              style={{ marginBottom: 15 }}
+              placeholder=""
+              class="form-control"
+              required
+            />
+          </div>
+
+          <Button
+            type="primary"
+            className="my-3 mx-9"
+            style={{ marginLeft: "22rem" }}
+            onClick={() => {
+              const name = document.getElementById("").value;
+
+              if (name == "") {
+                NotificationManager.warning("");
+              } else {
+                //   Add();
+              }
+            }}
+          >
+            ADD
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
 const Home = () => {
-    return (
+  return (
+    <div>
       <div>
         <div>
-          <div>
-            <All/>
-          </div>
+          <NewGass />
         </div>
       </div>
-    );
-  };
-  
-  export default Home;
+    </div>
+  );
+};
+
+export default Home;
