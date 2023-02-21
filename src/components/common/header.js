30b5f8eda60fc7dda316082ee243fd40219b3983
Shavidini Ekanayake <77123486+ShavidiniEkanayake@users.noreply.github.com>
2023-02-21 12:34:52 +0530

    iitialize project

---
+++ src/components/common/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useState, useContext } from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div
+        className={`w-full flex flex-col md:flex-row justify-between items-center bg-black p-5 py-5 fixed top-0 z-50 backdrop-blur-[5px] border-b border-white/10`}
+      >
+        <div className="md:w-5/12 pl-2 md:pl-12">
+          <img src="/assets/logo.png" className="w-8 h-8" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/pages/home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Modal, InputNumber, Space,Button } from "antd";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const All = () => {
+    return (
+    <div>aaa</div>
+    
+    )
+}
+
+const Home = () => {
+    return (
+      <div>
+        <div>
+          <div>
+            <All/>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Home;
